2751fad8af7f05cff2c9d27fa88cdf7c047f7419
Florent Lin <florentlin94000@gmail.com>
2025-02-14 13:52:18 +0100

    seconde partie

--- js/main.js
+++ js/main.js
@@ -5,6 +5,20 @@ console.log("Target confirmed")
 gsap.registerPlugin(ScrollTrigger,Observer,ScrollToPlugin,Draggable,MotionPathPlugin);
 
 
+
+// GSAP matchMedia responsive ------------------------------------------------------
+let mm = gsap.matchMedia();
+mm.add({
+    isMobile: '(max-width: 799px)', 
+    isDesktop: '(min-width: 800px)',
+}, (context) => {
+    let {isMobile, isDesktop} = context.conditions; 
+}
+)
+
+
+
+
 // GSAP DEMO ANIMATION
 gsap.fromTo("#demo",{
     rotation : 180,
@@ -36,13 +50,14 @@ gsap.to(".object",{
 
 
 //GSAP ANIMATION - MOTION PATH
-gsap.to(".video",{
+
+/*gsap.to(".ex4",{
     duration: 8,
     motionPath: {
     path:"M427 1C346.166 16.3333 184.5 104.8 184.5 336C184.5 567.2 335.5 568.5 427 578.5C545 578.5 683.8 597.2 659 424C628 207.5 370 217.5 365 377.5C361 505.5 363.333 857.167 365 1017C349.4 1200.6 176.5 1323.83 91.9999 1362.5C-177 1449.5 210.1 1675.8 478.5 1651C746.9 1626.2 710.667 1448.33 659 1362.5C629.833 1302.17 542.6 1196 427 1254C311.4 1312 306.833 1725.17 319 1924.5V2172",
     }
 })
-
+*/
 
 
 //GSAP ANIMATION TIMELINE
@@ -55,3 +70,16 @@ timelineex4.to('.ex4',{x:100,})
 .to(".yeah",{
     opacity:1,
 })
+
+Draggable.create(".text1",{
+
+    type: "x,y",
+
+    onDrag: function(){
+        document.querySelector(".text1").style.width = "100px";
+    },
+
+    onDragEnd: function(){
+        document.querySelector(".text1").style.width = "500px";
+    }
+});
